30dc3db56bf51ce5f6ee96d83eeb46e4955927e8
SEOKKAMONI <sj01048260279@gmail.com>
2023-05-25 14:12:04 +0900

    ADD :: Image 컴포넌트

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { color } from "../styles/color";
+
+const MainLayout = () => {
+  return <StyledMainLayout>as</StyledMainLayout>;
+};
+
+export default MainLayout;
+
+const StyledMainLayout = styled.section`
+  background-color: #f5f5f5;
+  width: 100vw;
+  height: 100vh;
+`;
---
+++ src/styles/color.ts
@@ -0,0 +1,5 @@
+export const color = {
+  black: "#000000",
+  white: "#FFFFFF",
+  orange: "#FF540F",
+};
