95dcd7986360c9132491d46b3973f84eb1f7271e
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-28 23:31:03 +0900

    ADD :: 검색기능

--- src/components/main/SearchInput.tsx
+++ src/components/main/SearchInput.tsx
@@ -1,13 +1,28 @@
 import { color } from "styles/color";
 import styled from "styled-components";
 import SearchIcon from "./Icon/Search";
+import { Navigate, useNavigate } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 
 const SearchInput = () => {
+  const navigate = useNavigate();
+  const [searchResult, setSearchResult] = useState("");
+  const onClick = () => {
+    navigate(`/search/result/${searchResult}`);
+    setSearchResult("");
+  };
+
   return (
     <SearchInputSection>
       <StyledSearchInput>
-        <Input placeholder="궁금한 시술, 병원 이름을 검색해보세요" />
-        <SearchIcon cursor="pointer" />
+        <Input
+          placeholder="궁금한 시술, 병원 이름을 검색해보세요"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchResult(e.target.value)
+          }
+          value={searchResult}
+        />
+        <SearchIcon cursor="pointer" onClick={onClick} />
       </StyledSearchInput>
     </SearchInputSection>
   );
